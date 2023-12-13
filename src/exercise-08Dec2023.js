5e629a175412e3ab7852abc15ed244153a1ae803
abhirai143 <abhishekraibagi81@gmail.com>
2023-12-13 10:35:50 +0530

    complete task

--- src/exercise-08Dec2023.js
+++ src/exercise-08Dec2023.js
@@ -9,26 +9,30 @@ import('./moviesPlay.js')
 function run() {
 	const movies = data.movies;
 	console.log('Number of movies: ' + movies.length);
-	// for (let i = 0; i < movies.length; i++) {
-	// 	// console.log(movies[i].originalLanguage);
-	// 	let tableHTML = "<table class='ui celled table'>";
-	// 	tableHTML += "<thead><tr><th>Language</th><th>Count</th></tr></thead>";
-	// 	tableHTML += "<tbody>";
-	// 	tableHTML += `<tr><td>${movies[i].originalLanguage} (${movies[i].originalLanguage})</td></tr>`;	
-	// 	tableHTML += "</tbody></table>";
-	// 	document.body.innerHTML = tableHTML;
 
-		
-	// }
+	let map = new Map();
+
 	const movieInfoDiv = document.getElementById('table');
-	let tableHTML = '';
+
 	const mpp = movies.map((object) =>{
-		console.log(object)
-		tableHTML += "<thead><tr><th>Language</th><th>Count</th></tr></thead>";
+		// let count = 0;
+
+		if (map.get(object.originalLanguage)){
+			map.set(object.originalLanguage, map.get(object.originalLanguage)+1)
+		}
+		else {
+			map.set(object.originalLanguage, 1)
+		}
+
+	})
+	let tableHTML = '';
+	tableHTML += "<thead><tr><th>Language</th><th>Count</th></tr></thead>";
+	map.forEach((key, value) =>{
 		tableHTML += "<tbody>";
-		tableHTML += `<tr><td>${object.originalLanguage} (${object.spokenLanguages})</td><td>here i have to display count</td></tr>`;	
+		tableHTML += `<tr><td>${value}</td><td>${key}</td></tr>`;	
 		tableHTML += "</tbody>"
 	})
 	movieInfoDiv.innerHTML = "<table class='ui celled table'>" + tableHTML +"</table>";
+
 
 }
